270e9b08298e25bb3799f758a1b39d200940b4e1
abdur1766 <mdaabdur71@gmail.com>
2025-08-31 00:44:46 +0600

    Update and rename ryuko/catalogs/ryukoa.js to rahim.js

--- rahim.js
+++ rahim.js
@@ -3,24 +3,29 @@ const { spawn } = require("child_process");
 const express = require("express");
 const app = express();
 const chalk = require('chalk');
-const logger = require("./ryukoc.js");
+const logger = require("./rahimc.js");
 const path = require('path');
 const PORT = process.env.PORT || 8080 || 9000 || 5555 || 5050 || 5000 || 3003 || 2000 || 1029 || 1010;
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/website/ryuko.html'));
+  res.sendFile(path.join(__dirname, '/website/rahim.html'));
 });
+
 console.clear();
+
 function startBot(message) {
     (message) ? logger(message, "starting") : "";
-  console.log(chalk.blue('DEPLOYING MAIN SYSTEM'));
-  logger.loader(`deploying app on port ${chalk.blueBright(PORT)}`);
-  app.listen(logger.loader(`app deployed on port ${chalk.blueBright(PORT)}`));
-  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "ryukob.js"], {
+    console.log(chalk.blue('DEPLOYING MAIN SYSTEM'));
+    logger.loader(`deploying app on port ${chalk.blueBright(PORT)}`);
+    app.listen(logger.loader(`app deployed on port ${chalk.blueBright(PORT)}`));
+    
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "rahimb.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
     });
-  child.on("close", (codeExit) => {
+
+    child.on("close", (codeExit) => {
         if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
             startBot();
             global.countRestart += 1;
@@ -28,8 +33,9 @@ function startBot(message) {
         } else return;
     });
 
-  child.on("error", function(error) {
-    logger("an error occurred : " + JSON.stringify(error), "error");
-  });
+    child.on("error", function(error) {
+        logger("an error occurred : " + JSON.stringify(error), "error");
+    });
 };
+
 startBot();
